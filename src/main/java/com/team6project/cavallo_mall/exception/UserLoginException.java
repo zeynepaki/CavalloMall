f117a5936821e384d120ee7db019b3572cc802a4
paulrobertwillis <49000144+paulrobertwillis@users.noreply.github.com>
2021-03-22 19:33:38 +0000

    TODOs added

--- src/main/java/com/team6project/cavallo_mall/exception/UserLoginException.java
+++ src/main/java/com/team6project/cavallo_mall/exception/UserLoginException.java
@@ -7,4 +7,5 @@ package com.team6project.cavallo_mall.exception;
  * date: 2021/2/18 1:42
  */
 public class UserLoginException extends RuntimeException {
+    // TODO: 22/03/2021 should there be exceptions here to be thrown?
 }
